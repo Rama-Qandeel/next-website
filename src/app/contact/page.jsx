88e10529081ae63969daa7056dac277b3f5415dc
Rama-qndeel <r.qandeel@emma.aero>
2023-11-05 12:27:32 +0300

    create main pages

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React from "react";
+import styles from "./page.module.css";
 
 const Contact = () => {
-  console.log('hello')
   return (
-    <div>
-      Contact
+    <div className={styles.contact}>
+      <div className={styles.left}>
+        <header className={styles.header}>
+          My <span>Details</span> & <span>Contact Information</span>
+        </header>
+        <div className={styles.contact_details}>
+          <span className={styles.span}>(+962) 799137234</span>
+        </div>
+        <div className={styles.contact_details}>
+          <span className={styles.span}>Jordan / Amman</span>
+        </div>
+        <div className={styles.contact_details}>
+          <span className={styles.span}>rama.qndeel@gmail.com</span>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <form className={styles.form}>
+            <input
+              type="text"
+              placeholder="FisrtName"
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="LastName"
+              className={styles.input}
+            />
+            <input type="email" placeholder="Email" className={styles.input} />
+            <input type="text" placeholder="Subject" className={styles.input} />
+            <textarea
+              placeholder="Message"
+              className={styles.textarea}
+            ></textarea>
+            <button className={styles.button}>Send</button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const Category = () => {
+const Category = ({params}) => {
+  console.log(params,'params')
   return (
     <div>
       Category
