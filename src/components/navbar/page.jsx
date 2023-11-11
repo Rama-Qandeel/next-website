24a862bddf5c9481be6cfca08d7ee874bb84f826
Rama-qndeel <r.qandeel@emma.aero>
2023-11-11 23:41:33 +0300

    sign in with email & password

--- src/components/navbar/page.jsx
+++ src/components/navbar/page.jsx
@@ -1,24 +1,32 @@
+"use client";
+
 import React from "react";
 import { links } from "@/constant";
 import Link from "next/link";
 import styles from "./page.module.css";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
-
+import { signOut, useSession } from "next-auth/react";
 
 const Navbar = () => {
+  const session = useSession();
+
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
         Rama
       </Link>
       <div className={styles.links}>
-        <DarkModeToggle/>
+        <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
-        <button className={styles.logout}>Logout</button>
+        {session.status === "authenticated" && (
+          <button className={styles.logout} onClick={signOut}>
+            Logout
+          </button>
+        )}{" "}
       </div>
     </div>
   );
